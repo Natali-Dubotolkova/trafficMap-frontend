094861a1ed93451299b721a57260d06e59bf0a5f
Natalie Dubotolkova <natali_dnd@mail.ru>
2022-12-15 11:32:43 +0300

    first commit

---
+++ src/common/auth-verify.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { withRouter } from "./with-router";
+
+const parseJwt = (token) => {
+  try {
+    console.log(JSON.parse(atob(token.split('.')[1])));
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
+
+const AuthVerify = (props) => {
+  let location = props.router.location;
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      const decodedJwt = parseJwt(user.token);
+
+      if (decodedJwt.exp * 1000 < Date.now()) {
+        props.logOut();
+      }
+    }
+  }, [location]);
+
+  return <div></div>;
+};
+
+export default withRouter(AuthVerify);
